5d7caf150a166fafb134a1bf3a2188bf03084e6a
smile995 <ah.smile995@gmai.com>
2023-07-30 17:02:44 +0600

    module2.2

---
+++ Module2/array.js
@@ -0,0 +1,11 @@
+var ex_friend=["Rakib", "Mukabbir", "Asraful", "Mahabub"];
+var versity_friend= ["Saim", 'Mehedi','Raihan', ];
+console.log (ex_friend);
+console.log (ex_friend.length);
+console.log (versity_friend);
+console.log (versity_friend.length);
+var vowel=['a', 'e', 'i', 'o', 'u'];
+console.log(vowel);
+console.log(vowel.length);
+var number=[1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+console.log (number.length)
