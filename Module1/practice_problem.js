48a0217c8ab314c1b22c2100229955c9ddb961e9
smile995 <ah.smile995@gmai.com>
2023-07-30 16:11:39 +0600

    module 1 done

---
+++ Module1/practice_problem.js
@@ -0,0 +1,40 @@
+// practice_problem 1:find total cost and that you have
+var totalAmount=1000;
+var apple=300;
+var orange=400;
+var totalCost=apple+orange;
+console.log(totalCost);
+var seller_Give=totalAmount-totalCost;
+console.log(seller_Give);
+// declared all the variable in a line
+var totalAmount, apple, orange, totalCost; seller_Give;
+totalAmount=1000;
+apple=300;
+orange=400;
+totalCost=apple+orange;
+console.log(totalCost);
+seller_Give=totalAmount-totalCost;
+console.log(seller_Give);
+
+// practice_problem 2:find the avarage mark
+var math=75.25;
+var biology=65.85;
+var chemestry=35.45;
+var bangla=99.50;
+var totalMark=math+biology+chemestry+bangla;
+console.log(totalMark);
+var avarageMark=totalMark/4;
+console.log(avarageMark );
+
+// practice_problem3: summation of string
+var going="I am going to be";
+var target="an awesome web devoloper";
+var sum_string= going+ " " + target;
+console.log(sum_string);
+
+
+// practice_problem:4 find reminder value
+var mark=119;
+var divider=5;
+var reminder=mark%divider;
+console.log(reminder);
